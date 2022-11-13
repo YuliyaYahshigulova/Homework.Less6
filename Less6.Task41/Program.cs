97c01c4f11f22d7bf83df8daa5017f12a51a9077
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-13 18:32:21 +0700

    dicided Task41 fnd Task43

---
+++ Less6.Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+int Elements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+int count = Elements(array);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
