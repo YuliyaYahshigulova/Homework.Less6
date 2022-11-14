1677fda0958294b14cf177da76235a72f82080c9
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-14 11:11:59 +0700

    redecided task41

--- Less6.Task41/Program.cs
+++ Less6.Task41/Program.cs
@@ -1,16 +1,102 @@
-﻿Console.Write("Введите элементы(через пробел): ");
-int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
 
-int Elements(int[] array)
+
+double[] CreateArray(int size)
+{
+    double[] element = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число:");
+        element[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return element;
+}
+
+int CountElements(double[] coll)
 {
     int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    for (int j = 0; j < coll.Length; j++)
     {
-        if (array[i] > 0) count++;
+        if (coll[j] > 0) count++;
     }
     return count;
 }
-int count = Elements(array);
-Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+string Print(int count, double[] massiv)
+{
+    return ("Количество элементов больше 0: " + count);
+}
+
+ int WriteConsole()
+{
+    Console.Write("Задайте количество элементов в массиве: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int n = WriteConsole();
+
+double[] array = CreateArray(n);
+int count = CountElements(array);
+Console.WriteLine(Print(count, array));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.Write("Введите элементы(через пробел): ");
+// int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse)!;
+// int Elements(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0) count++;
+//     }
+//     return count;
+// }
+// int count = Elements(array);
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
 
 
