50b87027949fde9e12574fd77f6099d0ee37a44d
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-14 16:29:36 +0700

    redecided task43

--- Less6.Task43/Program.cs
+++ Less6.Task43/Program.cs
@@ -2,22 +2,33 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 
+// Координата X т. пересечения:
 
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+double countX(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+// // Координата Y т. пересечения:
+
+double countY(double b1, double k1, double x)
+{
+    return k1 * x + b1;
+}
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = countX(b1, k1, b2, k2);
+double y = countY(b1, k1, x);
+Console.WriteLine($"({x},{y})");
 
